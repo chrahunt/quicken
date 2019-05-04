39be91c4fefa9eef7629cb54020b9e889032c041
Chris Hunt <chrahunt@gmail.com>
2019-05-04 18:11:40 -0400

    Add quicken.is_importing helper.
    Fixes #9.

--- quicken/__init__.py
+++ quicken/__init__.py
@@ -1,6 +1,17 @@
 """
-
 """
+from __future__ import annotations
+
 # Top-level package should be free from any unnecessary imports, since they are
 # unconditional.
 __version__ = '0.1.0'
+
+is_importing: bool = False
+"""Whether Quicken is currently doing imports and the app would benefit
+from not doing deferred loads.
+"""
+
+
+def set_importing(v: bool):
+    global is_importing
+    is_importing = v
